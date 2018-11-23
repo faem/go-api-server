645862bad937fdd837fe59be74647e334181f6d9
Fahim Abrar <fahimabrar@appscode.com>
2018-11-23 12:40:24 +0600

    updated main.go & curl.md

--- curl.md
+++ curl.md
@@ -1,6 +1,19 @@
+# CURL commands
+
 ```
-curl -i -X GET <link>
-curl -i -X GET <link>
-curl -i -X DELETE <link>
-curl -i -X POST -H 'Content-Type: application/json' -d '<json data>' <link>
-curl -i -X PUT -H 'Content-Type: application/json' -d '<json data>' <link>```
+curl -s -X GET <link>
+curl -s -X GET <link>
+curl -s -X DELETE <link>
+curl -s -X POST -H 'Content-Type: application/json' -d '<json data>' <link>
+curl -s -X PUT -H 'Content-Type: application/json' -d '<json data>' <link>
+```
+
+## Demo Curl commands
+
+```
+curl -s -X GET localhost:8080/in | jq
+curl -s -X GET localhost:8080/in/masud-rahman | jq
+curl -s -X DELETE localhost:8080/in/masud-rahman | jq
+curl -s -X POST -H 'Content-Type: application/json' -d '{"id":"kfoozminus","name":"Jannatul Ferdous","company":"AppsCode Inc.","position":"Software Engineer","skill":[{"name":"C++","noOfEndorsement":100},{"name":"C","noOfEndorsement":100}]}' localhost:8080/in | jq
+curl -s -X PUT -H 'Content-Type: application/json' -d '{"id":"masud-rahman","name":"Masudur Rahman (modified)","company":"AppsCode Inc.","position":"Software Engineer","skill":[{"name":"C","noOfEndorsement":3},{"name":"C++","noOfEndorsement":4}]}' localhost:8080/in/masud-rahman | jq
+```
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/davecgh/go-spew/spew"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -21,69 +21,111 @@ type Skill struct {
 	NoOfEndorsement int    `json:"noOfEndorsement"`
 }
 
+type Admin struct {
+	UserName string `json:"username"`
+	Password string `json:"password"`
+}
+
+//map for our demo database, used map key as ID
 var profilesDB map[string]Profile
 
+//used to get all the profile info using GET request
 func GetProfiles(w http.ResponseWriter, r *http.Request) {
-	if !BasicAuth(){
+	if !BasicAuth() {
 		return
 	}
 	var profiles []Profile
-	//spew.Dump(profilesDB==nil)
+
 	for _, profile := range profilesDB {
 		profiles = append(profiles, profile)
 	}
+	//shows error if no content in the DB
+	if profiles == nil {
+		w.WriteHeader(http.StatusNoContent)
+		w.Write([]byte(fmt.Sprintf("No content found!")))
+		return
+	}
 
-	spew.Dump(json.NewEncoder(w).Encode(profiles))
+	json.NewEncoder(w).Encode(profiles)
 	//w.Write([]byte(fmt.Sprintf("%v", profiles)))
 }
 
+//used to get a specific profile info using GET request
 func GetProfile(w http.ResponseWriter, r *http.Request) {
-	if !BasicAuth(){
+	if !BasicAuth() {
 		return
 	}
+
 	params := mux.Vars(r)
-	//spew.Dump(params)
-	//spew.Dump("GP")
+
+	if _, flag := profilesDB[params["id"]]; !flag {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("Profile of " + string(params["id"]) + " not found!")))
+		return
+	}
+
 	json.NewEncoder(w).Encode(profilesDB[params["id"]])
 }
 
+//used to delete a profile using DELETE request
 func DeleteProfile(w http.ResponseWriter, r *http.Request) {
-	if !BasicAuth(){
+	if !BasicAuth() {
 		return
 	}
-	//spew.Dump("delete Profile")
+
 	params := mux.Vars(r)
-	//spew.Dump(params["id"])
+	if _, flag := profilesDB[params["id"]]; !flag {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("Profile of " + string(params["id"]) + " not found!")))
+		return
+	}
+
 	delete(profilesDB, params["id"])
 }
 
+//used to add new profile using POST request
 func AddProfile(w http.ResponseWriter, r *http.Request) {
-	if !BasicAuth(){
+	if !BasicAuth() {
 		return
 	}
+
 	var profile Profile
 	json.NewDecoder(r.Body).Decode(&profile)
-	profilesDB[profile.ID] = profile
-	spew.Dump(profile)
-}
 
-func UpdateProfile(w http.ResponseWriter, r *http.Request) {
-	if !BasicAuth(){
+	if _, flag := profilesDB[profile.ID]; !flag {
+		w.WriteHeader(http.StatusConflict)
+		w.Write([]byte(fmt.Sprintf("Username " + string(profile.ID) + " already exists!")))
 		return
 	}
+
+	profilesDB[profile.ID] = profile
+}
+
+//used to update a profile using PUT request
+func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+	if !BasicAuth() {
+		return
+	}
+
 	params := mux.Vars(r)
-	spew.Dump(params)
-	//spew.Dump(r.Body)
+
+	if _, flag := profilesDB[params["id"]]; !flag {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("Profile of " + string(params["id"]) + " not found!")))
+		return
+	}
+
 	var profile Profile
 	json.NewDecoder(r.Body).Decode(&profile)
 	profilesDB[params["id"]] = profile
-	spew.Dump(profile)
 }
 
-func BasicAuth() bool{
+//basic authentication function
+func BasicAuth() bool {
 	return true
 }
 
+//this function creates a demo DB for our server
 func CreateDemoDB() {
 	profilesDB = make(map[string]Profile)
 	profilesDB["fahim-abrar"] = Profile{
@@ -115,6 +157,26 @@ func CreateDemoDB() {
 			{
 				"C++",
 				4,
+			},
+		}}
+
+	profilesDB["mohan"] = Profile{
+		"mohan",
+		"Tahsin Rahman",
+		"AppsCode Inc.",
+		"Software Engineer",
+		[]Skill{
+			{
+				"C",
+				100,
+			},
+			{
+				"C++",
+				110,
+			},
+			{
+				"Linux",
+				100,
 			},
 		}}
 }
